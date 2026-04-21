d9c9029f30c0312b95aafde439a8d2560b26be32
FelippeMenezes <42714571+FelippeMenezes@users.noreply.github.com>
2026-04-21 17:34:42 -0300

    Player variable name and role created with attr accessor

--- lib/players.rb
+++ lib/players.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class Player
+  attr_accessor :name, :role
 
-  def initialize(name, team)
+  def initialize(name, role)
     @name = name
-    @team = team
+    @role = role
   end
 end
+
+player_one = Player.new("Felippe", "X")
+p player_one.name
