90de5fbda834224621621d057fc32508f03faaa0
FelippeMenezes <42714571+FelippeMenezes@users.noreply.github.com>
2026-04-21 20:58:35 -0300

    Created Class Match

--- lib/boards.rb
+++ lib/boards.rb
@@ -10,9 +10,9 @@ class Board
   end
 
   def self.create_board
-    line_1 = ["A1", "A2", "A3"]
-    line_2 = ["B1", "B2", "B3"]
-    line_3 = ["C1", "C2", "C3"]
+    line_1 = ["1", "2", "3"]
+    line_2 = ["4", "5", "6"]
+    line_3 = ["7", "8", "9"]
     Board.new(line_1, line_2, line_3)
   end
 end
--- main.rb
+++ main.rb
@@ -3,11 +3,21 @@
 require_relative './lib/players'
 require_relative './lib/boards'
 
+puts "Player 1 - What's is yor name?"
+player_one_name = gets.chomp
+puts "#{player_one_name}, choose your role('x' or 'o'):"
+player_one_role = gets.chomp
+
+player_one = Player.new(player_one_name, player_one_role)
+puts "#{player_one.name}, You will play with '#{player_one.role}'!"
+
+puts "Player 2 - What's is yor name?"
+player_two_name = gets.chomp
+player_one.role == "o" ? player_two_role = "x" : player_two_role = "o"
+
+player_two = Player.new(player_two_name, player_two_role)
+puts "#{player_two.name}, You will play with '#{player_two.role}'!"
+
 board = Board.create_board
+p board.line_1, board.line_2, board.line_3
 
-p board.line_1
-p board.line_2
-p board.line_3
-
-player_one = Player.new("Felippe", "X")
-puts player_one.name
