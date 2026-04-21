c34f8777b79b67c434d745cf6482635e98e9108a
FelippeMenezes <42714571+FelippeMenezes@users.noreply.github.com>
2026-04-21 18:06:33 -0300

    #create_board implemented

--- lib/boards.rb
+++ lib/boards.rb
@@ -8,4 +8,11 @@ class Board
     @line_2 = line_2
     @line_3 = line_3
   end
+
+  def self.create_board
+    line_1 = ["A1", "A2", "A3"]
+    line_2 = ["B1", "B2", "B3"]
+    line_3 = ["C1", "C2", "C3"]
+    Board.new(line_1, line_2, line_3)
+  end
 end
--- main.rb
+++ main.rb
@@ -3,5 +3,11 @@
 require_relative './lib/players'
 require_relative './lib/boards'
 
+board = Board.create_board
+
+p board.line_1
+p board.line_2
+p board.line_3
+
 player_one = Player.new("Felippe", "X")
 puts player_one.name
