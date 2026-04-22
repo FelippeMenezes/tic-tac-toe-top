88d58683fb24e9d414243a3b52bc8595d9d59c5c
FelippeMenezes <42714571+FelippeMenezes@users.noreply.github.com>
2026-04-22 09:26:13 -0300

    instance variable 'match' has been created, and players can play a match

--- main.rb
+++ main.rb
@@ -16,4 +16,5 @@ player_two.set_player_two_role(player_one, player_two)
 board = Board.create_board
 board.show_board
 
-play_match(player_one, player_two, board)
+match = Match.new(player_one, player_two, board)
+match.play_match(player_one, player_two, board)
