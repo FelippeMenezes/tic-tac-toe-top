343fcc03fd17848a4c2788df4c4840ac0bb8c279
FelippeMenezes <42714571+FelippeMenezes@users.noreply.github.com>
2026-04-21 23:23:13 -0300

    Players methods

--- lib/players.rb
+++ lib/players.rb
@@ -3,8 +3,36 @@
 class Player
   attr_accessor :name, :role
 
-  def initialize(name, role)
+  def initialize(name = nil, role = nil)
     @name = name
     @role = role
+  end
+
+  def ask_player_one_name(player)
+    puts "Player 1 - What's is yor name?"
+    player.name = gets.chomp
+    player.name
+  end
+
+  def ask_player_two_name(player)
+    puts "Player 2 - What's is yor name?"
+    player.name = gets.chomp
+    player.name
+  end
+
+  def ask_player_one_role(player)
+    puts "#{player.name}, choose your role('x' or 'o'):"
+    player.role = gets.chomp
+    puts "#{player.name}, you will play with '#{player.role}'"
+    player.role
+  end
+
+  def set_player_two_role(player_one, player_two)
+    if player_one.role == "x"
+      player_two.role = "o"
+    else
+      player_two.role = "x"
+    end
+    puts "#{player_two.name}, you will play with '#{player_two.role}'"
   end
 end
--- main.rb
+++ main.rb
@@ -2,22 +2,18 @@
 
 require_relative './lib/players'
 require_relative './lib/boards'
+require_relative './lib/matches'
 
-puts "Player 1 - What's is yor name?"
-player_one_name = gets.chomp
-puts "#{player_one_name}, choose your role('x' or 'o'):"
-player_one_role = gets.chomp
+player_one = Player.new
+player_two = Player.new
 
-player_one = Player.new(player_one_name, player_one_role)
-puts "#{player_one.name}, You will play with '#{player_one.role}'!"
+player_one.name = player_one.ask_player_one_name(player_one)
+player_one.role = player_one.ask_player_one_role(player_one)
 
-puts "Player 2 - What's is yor name?"
-player_two_name = gets.chomp
-player_one.role == "o" ? player_two_role = "x" : player_two_role = "o"
-
-player_two = Player.new(player_two_name, player_two_role)
-puts "#{player_two.name}, You will play with '#{player_two.role}'!"
+player_two.name = player_two.ask_player_two_name(player_two)
+player_two.set_player_two_role(player_one, player_two)
 
 board = Board.create_board
 p board.line_1, board.line_2, board.line_3
 
+play_match(player_one, player_two, board)
